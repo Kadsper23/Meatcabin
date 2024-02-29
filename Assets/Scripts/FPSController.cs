3df9ce9a499bac798f6f0b2a76af9a7abcb5b12e
Kadsper23 <kadensperry7@gmail.com>
2024-02-29 13:21:40 -0700

    textures

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -11,61 +11,40 @@ public class FPSController : MonoBehaviour
     public float jumpPower = 7f;
     public float gravity = 10f;
 
-
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
-
 
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
 
     public bool canMove = true;
 
-
     CharacterController characterController;
+
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+
+        // Lock cursor in the center and hide it
+        LockCursor();
     }
 
     void Update()
     {
+        // Handle rotation
+        HandleRotation();
 
-        #region Handles Movment
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 right = transform.TransformDirection(Vector3.right);
-
-        // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
-
-        #endregion
-
-        #region Handles Jumping
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        // Handle movement
+        if (canMove)
         {
-            moveDirection.y = jumpPower;
-        }
-        else
-        {
-            moveDirection.y = movementDirectionY;
+            HandleMovement();
+            HandleJumping();
         }
 
-        if (!characterController.isGrounded)
-        {
-            moveDirection.y -= gravity * Time.deltaTime;
-        }
+    }
 
-        #endregion
-
-        #region Handles Rotation
-        characterController.Move(moveDirection * Time.deltaTime);
-
+    void HandleRotation()
+    {
         if (canMove)
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
@@ -73,7 +52,36 @@ public class FPSController : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
+    }
 
-        #endregion
+    void HandleMovement()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical");
+        float curSpeedY = (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal");
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    void HandleJumping()
+    {
+        if (characterController.isGrounded && Input.GetButton("Jump"))
+        {
+            moveDirection.y = jumpPower;
+        }
+    }
+
+
+
+    // Lock cursor in the center and hide it
+    void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,14 +6,40 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void PlayGame()
+    void Start()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex * 1);
+        // Show cursor and unlock it when entering the main menu
+        SetCursorVisibility(true);
     }
 
-    // Update is called once per frame
+    public void PlayGame()
+    {
+        // Transition to gameplay scene
+        SceneManager.LoadScene("Cabin");
+
+        // Hide cursor for gameplay
+        SetCursorVisibility(false);
+    }
+
     public void QuitGame()
     {
+        // Quit application
         Application.Quit();
     }
+
+    // Helper method to set cursor visibility and lock state
+    private void SetCursorVisibility(bool isVisible)
+    {
+        if (isVisible)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
 }
+
